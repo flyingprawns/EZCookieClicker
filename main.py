40a361b6b5de55c8f4439282c537d46f68878bb0
Qijie Zhu <qzhu9@ucsc.edu>
2021-11-18 19:46:05 -0800

    program can now access shop prices

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
-# Open cookie clicker page in Selenium
+# --------------  Open cookie clicker page in Selenium --------------- #
 s = Service("C:/Users/Jack/Documents/Development/chromedriver.exe")
 driver = webdriver.Chrome(service=s)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 assert "Cookie" in driver.title
 
-# Find and save essential UI elements
+# -------------- Find and save essential UI elements -------------- #
 cookie = driver.find_element(By.ID, "cookie")
-money = driver.find_element(By.ID, "money")
+cookieCount = driver.find_element(By.ID, "money")
 cursor = driver.find_element(By.ID, "buyCursor")
 grandma = driver.find_element(By.ID, "buyGrandma")
 factory = driver.find_element(By.ID, "buyFactory")
@@ -20,24 +20,44 @@ shipment = driver.find_element(By.ID, "buyShipment")
 alchemy = driver.find_element(By.ID, "buyAlchemy lab")
 portal = driver.find_element(By.ID, "buyPortal")
 timeMachine = driver.find_element(By.ID, "buyTime machine")
+shop = [timeMachine, portal, alchemy, shipment, mine, factory, grandma, cursor]
 
-# # Play the game
-# CLICK_INTERVAL = 0.00001
-# CYCLES_TIL_UPGRADE = 501
-# cycle = 0
-# while True:
-#     # Click on cookie
-#     time.sleep(CLICK_INTERVAL)
-#     cookie.click()
-#     # Occasionally check for upgrades
-#     cycle += 1
-#     if cycle >= CYCLES_TIL_UPGRADE:
-#         print("5s has passed")
-#         cycle = 0
+
+# -------------- Cookie clicker functions -------------- #
+def get_price(item):
+    # Parses the text field of an item and returns its price.
+    num_found = False
+    p = ""
+    for char in item.text:
+        if char.isnumeric():
+            p += char
+            num_found = True
+        elif num_found and char != ",":
+            break
+    return int(p)
 
 
 def upgrade():
-    print(money.text)
+    money = int(cookieCount.text)
+    for item in shop:
+        if get_price(item) <= money:
+            item.click()
+            item.click()
+            return
 
+
+# -------------- Play the game -------------- #
+CLICK_INTERVAL = 0.00001
+CYCLES_TIL_UPGRADE = 501
+cycle = 0
+while True:
+    # Click on cookie
+    time.sleep(CLICK_INTERVAL)
+    cookie.click()
+    # Occasionally check for upgrades
+    cycle += 1
+    if cycle >= CYCLES_TIL_UPGRADE:
+        print("5s has passed")
+        cycle = 0
 
 upgrade()
